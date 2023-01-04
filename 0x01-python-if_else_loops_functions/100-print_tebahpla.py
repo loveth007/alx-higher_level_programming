cf8975b507e8507256e1fcc374ffcfe5c15d9146
loveth007 <lovethokwudiri@gmail.com>
2023-01-04 00:11:34 +0000

    8 and 100 corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-index = 0
-for ch in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(ch - index)), end="")
-    index = 32 if index == 0 else 0
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
